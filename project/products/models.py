088101fcbb7536e8294d6c3b4f7305582ec80512
mohamekni <mekni.moha99@gmail.com>
2025-06-27 14:10:30 +0200

    Learn Django

--- project/products/models.py
+++ project/products/models.py
@@ -7,3 +7,10 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to='photos/%y/%m/%d')
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Phone'
+        ordering = ['-price']

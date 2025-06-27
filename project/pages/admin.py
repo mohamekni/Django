3ecfe8ac5f69f88d8186c231f2c397016beda969
mohamekni <mekni.moha99@gmail.com>
2025-06-27 17:25:55 +0200

    Learn Django

--- project/pages/admin.py
+++ project/pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Female, Male, User, Product, Video, SimpleUser
 # Register your models here.
+
+admin.site.register(Female)
+admin.site.register(Male)
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Video)
+admin.site.register(SimpleUser)
--- project/pages/models.py
+++ project/pages/models.py
@@ -2,4 +2,39 @@ from django.db import models
 
 # Create your models here.
 
+# One To One Relationship
+class Female(models.Model):
+    Female_name = models.CharField(max_length=100, null=True)
+    def __str__(self):
+        return self.Female_name
 
+class Male(models.Model):
+    Male_name = models.CharField(max_length=100, null=True)
+    girl = models.OneToOneField(Female, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.Male_name
+    
+
+# Many To One Relationship
+class Product(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    def __str__(self):
+        return self.title
+    
+class User(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    
+# Many To Many Relationship
+class Video(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    def __str__(self):
+        return self.title
+    
+class SimpleUser(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    watch = models.ManyToManyField(Video, null=True)
+    def __str__(self):
+        return self.name

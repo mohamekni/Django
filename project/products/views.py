51088b56a265871c16a45baac8face1be435b456
mohamekni <mekni.moha99@gmail.com>
2025-06-28 20:23:13 +0200

    Learn Django

--- project/products/views.py
+++ project/products/views.py
@@ -6,5 +6,18 @@ def product(request):
     return render(request, 'products/product.html')
 
 def products(request):
-    items = {'products': Product.objects.all().filter(price = 999)}    
+
+    # get(id=1)
+    # all()
+    # filter(price=850)
+    # order_by('price')
+    # count()
+    # exclude(price=850)
+    # filter(price__exact=850)
+    # filter(name__contains='pro')
+    # filter(price__in=[850, 999])
+    # filter(price__range=[900, 1000])
+
+    all_Items = Product.objects.all()
+    items = {'products': all_Items.filter(price__range=[900, 1000])}
     return render(request, 'products/products.html', items)

21544bdc01e64bffe5e17bbbcc511c05a1d4ccf5
mohamekni <mekni.moha99@gmail.com>
2025-06-29 18:52:13 +0200

    Learn Django

--- project/products/admin.py
+++ project/products/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Product, TestDate
 # Register your models here.
 
-admin.site.register(Product)
+class productAdmin(admin.ModelAdmin):
+    list_display = ['name', 'content', 'price','category','active']
+    list_display_links = ['price']
+    list_editable = ['name','category','active']
+    search_fields = ['name','content','price','category','active']
+    list_filter = ['price']
+    fields = ['name','price','active']
+
+admin.site.register(Product, productAdmin)
 admin.site.register(TestDate)
 admin.site.site_header = 'Admin Panel'
 admin.site.site_title = 'MohaSoGoodNoLove'
--- project/products/apps.py
+++ project/products/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+from suit.apps import DjangoSuitConfig
 
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
+
+
+class SuiteConfig(DjangoSuitConfig):
+    layout = 'vertical'
